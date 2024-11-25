9598b350d249132291926394f66cd1bb9456026f
as6620 <as6620@bs.amalnet.k12.il>
2024-11-25 15:18:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/customspnlst/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.customspnlst;
+
+import android.os.Bundle;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spin; // מערך של עצמים
+    TextView tV;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spin = (Spinner) findViewById(R.id.spin);
+        tV = (TextView) findViewById(R.id.tV);
+
+    }
+}

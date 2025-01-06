02e66a6cade07636f55f168fe0e820eb3e1b65ed
as6620 <as6620@bs.amalnet.k12.il>
2025-01-06 23:18:52 +0200

    custom adp

--- app/src/main/java/com/example/customspnlst/MainActivity.java
+++ app/src/main/java/com/example/customspnlst/MainActivity.java
@@ -6,30 +6,45 @@ import android.widget.AdapterView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spin;
     TextView tV;
     Country[] country = new Country[7];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spin = (Spinner) findViewById(R.id.spin);
-        tV = (TextView) findViewById(R.id.tV);
+        spin = findViewById(R.id.spin);
+        tV = findViewById(R.id.tV);
 
-        country[0] = new Country(R.drawable.isrFlag, "Israel", "Jerusalem", 12000000);
-        country[1] = new Country(R.drawable.italyFlag, "Italy", "Rome", 331000000);
-        country[2] = new Country(R.drawable.fraceFlag, "France", "Paris", 67000000);
-        country[3] = new Country(R.drawable.canadaFlag, "Canada", "Ottawa", 83000000);
-        country[4] = new Country(R.drawable.japanFlag, "Japan", "Tokyo", 3000000);
-        country[5] = new Country(R.drawable.russiaFlag, "Russia", "Moscow", 4000000);
-        country[6] = new Country(R.drawable.egyptFlag, "Egypt", "Cairo", 3000000);
+        country[0] = new Country(R.drawable.isr_flag, "Israel", "Jerusalem", 12000000);
+        country[1] = new Country(R.drawable.italy_flag, "Italy", "Rome", 60200000);
+        country[2] = new Country(R.drawable.frace_flag, "France", "Paris", 67000000);
+        country[3] = new Country(R.drawable.canada_flag, "Canada", "Ottawa", 38000000);
+        country[4] = new Country(R.drawable.japan_flag, "Japan", "Tokyo", 126000000);
+        country[5] = new Country(R.drawable.russia_flag, "Russia", "Moscow", 144000000);
+        country[6] = new Country(R.drawable.egypt_flag, "Egypt", "Cairo", 104000000);
+
+        spin.setOnItemSelectedListener(this);
+
+        CustomAdapter adp = new CustomAdapter(this, country);
+        spin.setAdapter(adp);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Country selectedCountry = country[position];
+        tV.setText("Country Name: " + selectedCountry.getCountry() +
+                "\nCapital City: " + selectedCountry.getCapital() +
+                "\nPopulation: " + selectedCountry.getPopulation());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        tV.setText("");
     }
 }
